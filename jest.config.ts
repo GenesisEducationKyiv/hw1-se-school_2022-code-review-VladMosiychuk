025a0d29469275b5bed22c05f6c2c632b522624b
Vlad Mosiychuk <mosyka007@gmail.com>
2022-09-19 22:17:50 +0300

    Methods from 'fs' module are mocked to ensure they are actually called to write emails on the disk.

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'jest'
 
 const config: Config = {
+  roots: ['src'],
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleNameMapper: {
--- src/service/__tests__/email.unit.ts
+++ src/service/__tests__/email.unit.ts
@@ -1,4 +1,5 @@
 import fs from 'fs/promises'
+import { constants } from 'fs'
 
 import { test, describe, expect } from '@jest/globals'
 import { getEmails, addNewEmail } from '../email/email.service'
@@ -6,22 +7,31 @@ import { getEmails, addNewEmail } from '../email/email.service'
 import config from '../../config'
 
 const testEmails = ['test1@gmail.com', 'test2@gmail.com']
+const accessFn = jest.spyOn(fs, 'access')
+const readFileFn = jest.spyOn(fs, 'readFile')
+const writeFileFn = jest.spyOn(fs, 'writeFile')
 
 describe('Testing Email Service', () => {
 
   beforeEach(() => {
-    fs.unlink(config.EMAILS_FN).catch(() => {})
+    fs.unlink(config.EMAILS_FN).catch(() => { })
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
   })
 
   test('if getEmails returns empty array by default', async () => {
     const emails = await getEmails()
 
+    expect(accessFn).toBeCalledWith(config.EMAILS_FN, constants.F_OK)
     expect(emails).toHaveLength(0)
   })
 
   test('if adding new email works', async () => {
     const emailAdded = await addNewEmail(testEmails[0])
 
+    expect(writeFileFn).toBeCalledWith(config.EMAILS_FN, expect.anything(), { flag: 'a' })
     expect(emailAdded).toBe(true)
   })
 
@@ -30,6 +40,8 @@ describe('Testing Email Service', () => {
 
     const emailAdded = await addNewEmail(testEmails[0])
 
+    expect(writeFileFn).toBeCalledTimes(1)
+    expect(readFileFn).toBeCalledWith(config.EMAILS_FN)
     expect(emailAdded).toBe(false)
   })
 
@@ -40,6 +52,7 @@ describe('Testing Email Service', () => {
 
     const emails = await getEmails()
 
+    expect(writeFileFn).toBeCalledTimes(testEmails.length)
     expect(emails).toEqual(testEmails)
   })
 })
